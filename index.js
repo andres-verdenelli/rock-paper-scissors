fdcec46ef1461da12a517deb53815719c33218aa
Andres Verdenelli <andresverdenelli@icloud.com>
2023-09-06 18:45:14 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// let playerName = prompt("What's your name?")
+let playerSelection = prompt('Rock, Paper or Scissors')
+let playerPoints = 0
+let computerPoints = 0
+let computerSelection = getComputerChoise()
+
+function getComputerChoise() {
+	const choise = ['Rock', 'Paper', 'Scissors']
+	return choise[Math.floor(Math.random() * choise.length)]
+}
+
+function playRound(playerChoise, computerChoise) {
+	let playerChoiseLower = playerChoise.toLowerCase()
+	let computerChoiseLower = computerChoise.toLowerCase()
+	if (playerChoiseLower === computerChoiseLower) {
+		console.log(`Tie, both choose ${playerChoise}`)
+	} else if (
+		(playerChoiseLower === 'rock' && computerChoiseLower === 'scissors') ||
+		(playerChoiseLower === 'paper' && computerChoiseLower === 'rock') ||
+		(playerChoiseLower === 'scissors' && computerChoiseLower === 'paper')
+	) {
+		console.log(`You win. ${playerChoise} win to ${computerChoise}`)
+	} else {
+		console.log(`Computer win.${computerChoise} win to ${playerChoise}`)
+	}
+}
+
+playRound(playerSelection, computerSelection)
